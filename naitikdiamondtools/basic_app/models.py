3766aa291c5f136d93310da0d3313be2b131baec
Naitik0909 <parmarnaitik0909@gmail.com>
2020-04-24 21:42:20 +0530

    3 new image fields added to models

--- naitikdiamondtools/basic_app/models.py
+++ naitikdiamondtools/basic_app/models.py
@@ -5,5 +5,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='images', blank=True)
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=1000)
+    coverimg1 = models.ImageField(upload_to='images/cover/1', blank=True)
+    coverimg2 = models.ImageField(upload_to='images/cover/2', blank=True)
+    coverimg3 = models.ImageField(upload_to='images/cover/3', blank=True)
     def __str__(self):
         return self.name
